33ee44a489eed97c3cd4259fa053037372710feb
Rayan Hatout <rayan.hatout@gmail.com>
2020-10-15 23:23:05 +0100

    Dynamically modify the system path at runtime to support custom component imports

--- src/app.py
+++ src/app.py
@@ -2,9 +2,13 @@
 
 # Run this app with `gunicorn app:server` and
 # visit http://127.0.0.1:8000/ in your web browser.
+import os
+import sys
 
 import dash
-from src.hierarchy_tree.HierarchyTree import HierarchyTree
+sys.path.append(os.path.join(os.path.dirname(__file__), 'hierarchy_tree'))
+print(sys.path)
+from hierarchy_tree.HierarchyTree import HierarchyTree
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
