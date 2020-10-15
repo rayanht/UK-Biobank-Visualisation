867796ee7416d610de5816f2073692c5685c6b7e
Rayan Hatout <rayan.hatout@gmail.com>
2020-10-15 15:58:25 +0100

    Add required statically generated __init__.py

--- src/components/hierarchy_tree/usage.py
+++ src/components/hierarchy_tree/usage.py
@@ -1,16 +1,15 @@
-import hierarchy_tree
 import dash
-from dash.dependencies import Input, Output
 import dash_html_components as html
+
+from src.components.hierarchy_tree.hierarchy_tree.HierarchyTree import HierarchyTree
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    hierarchy_tree.HierarchyTree(
+    HierarchyTree(
         id='input'),
     html.Div(id='output')
 ])
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
