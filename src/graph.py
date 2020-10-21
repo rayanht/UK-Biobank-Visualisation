de6d7fd4fbb166b1fa2b3948cdeb73ad40b445e0
lh5918 <lh5918@ic.ac.uk>
2020-10-21 16:10:24 +0800

    Changed format of options value to be consistent with node dbid

--- src/graph.py
+++ src/graph.py
@@ -45,11 +45,11 @@ class Graph:
         if (len(inst_names) > 1) :
             # There are multiple instances. Drop row with field name
             inst_names = inst_names.loc[inst_names['InstanceID'].notnull()]
-            inst_names['FieldInstanceID'] = str(field_id) + '-' + \
-                                            inst_names['InstanceID'].astype(str)
+            inst_names['dbid'] \
+                = inst_names.apply(lambda row:  f'_{field_id}_{int(row.InstanceID)}_0_', axis=1)
         else :
-            inst_names['FieldInstanceID'] = str(field_id) + '-0.0'
-        inst_name_dict = dict(zip(inst_names['FieldInstanceID'], inst_names['NodeName']))
+            inst_names['dbid'] = '_' + field_id + '_0_0'
+        inst_name_dict = dict(zip(inst_names['dbid'], inst_names['NodeName']))
         return inst_name_dict
 
     def get_field_instance_map(self, has_instances, has_array):
