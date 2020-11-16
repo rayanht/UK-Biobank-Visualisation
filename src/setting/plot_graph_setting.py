e8dc48193ba87e4afa0a3d177a462df19cc531c9
Richard Xiong <rjx18@ic.ac.uk>
2020-11-16 13:02:06 +0000

    Removed separate callback for statistics card because it fetches data twice for no reason

--- src/setting/plot_graph_setting.py
+++ src/setting/plot_graph_setting.py
@@ -27,11 +27,15 @@ def get_button(var=None):
 @app.callback(
     [
         Output(component_id="graph-data", component_property="data"),
+        Output(component_id="statistics", component_property="children"),
         Output(component_id="plotted-data", component_property="data"),
         Output(component_id="graph", component_property="figure"),
         Output(component_id="download-btn", component_property="disabled"),
     ],
-    [Input(component_id="settings-card-submit", component_property="n_clicks")],
+    [
+        Input(component_id="graph", component_property="selectedData"),
+        Input(component_id="settings-card-submit", component_property="n_clicks"),
+    ],
     [
         State(component_id="graph-data", component_property="data"),
         State(component_id=get_inst_dropdown_id("x"), component_property="value"),
@@ -46,16 +50,42 @@ def get_button(var=None):
         State(component_id=get_slider_id("y"), component_property="value"),
     ],
 )
-def get_data(n, cached_data, x_value, y_value, graph_type, colour, x_filter, y_filter):
-    data, new_cached_data, _, _ = get_data_from_settings(
-        cached_data, x_value, y_value, colour, x_filter, y_filter
-    )
+def get_data(
+    selected_data,
+    n,
+    cached_data,
+    x_value,
+    y_value,
+    graph_type,
+    colour,
+    x_filter,
+    y_filter,
+):
 
-    if data is None:
-        return (
-            new_cached_data,
-            None,
-            {
+    ctx = dash.callback_context
+
+    graph_data_update = dash.no_update
+    statistics_update = dash.no_update
+    plotted_data_update = dash.no_update
+    graph_figure_update = dash.no_update
+    download_btn_update = dash.no_update
+
+    if not ctx.triggered:
+        trigger = None
+    else:
+        trigger = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if trigger == "settings-card-submit":
+
+        data, new_cached_data, node_id_x, node_id_y = get_data_from_settings(
+            cached_data, x_value, y_value, colour, x_filter, y_filter
+        )
+
+        if data is None:
+            graph_data_update = new_cached_data
+            statistics_update = "No data to display"
+            plotted_data_update = None
+            graph_figure_update = {
                 "layout": {
                     "xaxis": {"visible": False},
                     "yaxis": {"visible": False},
@@ -69,20 +99,45 @@ def get_data(n, cached_data, x_value, y_value, graph_type, colour, x_filter, y_f
                         }
                     ],
                 }
-            },
-            True,
-        )
+            }
+            download_btn_update = True
 
-    # filter data
-    plotted_data_json, removed_eids = get_filtered_data(
-        data, x_value, y_value, x_filter, y_filter
-    )
+        else:
+            # filter data
+            plotted_data_json, removed_eids = get_filtered_data(
+                data, x_value, y_value, x_filter, y_filter
+            )
+            graph_data_update = new_cached_data
+            statistics_update = get_statistics(removed_eids, node_id_x, node_id_y)
+            plotted_data_update = plotted_data_json
+            graph_figure_update = get_field_plot(
+                removed_eids, x_value, y_value, colour, graph_type
+            )
+            download_btn_update = False
+
+    elif trigger == "graph" and selected_data:
+        points = selected_data["points"]
+        points = [(p["x"], p["y"]) for p in points]
+        points_x, points_y = zip(*points)
+        df = None
+        node_id_x = None
+        node_id_y = None
+
+        if x_value is not None:
+            node_id_x = NodeIdentifier(x_value)
+            df = pd.DataFrame({"x": points_x})
+        if y_value is not None:
+            node_id_y = NodeIdentifier(y_value)
+            df = pd.DataFrame({"x": points_x, "y": points_y})
+
+        statistics_update = get_statistics(df, node_id_x, node_id_y)
 
     return (
-        new_cached_data,
-        plotted_data_json,
-        get_field_plot(removed_eids, x_value, y_value, colour, graph_type),
-        False,
+        graph_data_update,
+        statistics_update,
+        plotted_data_update,
+        graph_figure_update,
+        download_btn_update,
     )
 
 
